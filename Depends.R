358c98b0cd47dfecb3756b0fada09e5f4c281b68
tcarruth <tomsinbox@gmail.com>
2019-09-25 03:01:09 -0700

    Other docs

---
+++ Depends.R
@@ -0,0 +1,17 @@
+
+# Installation script for ABTMSE dependencies
+
+packages<-c("roxygen2","snowfall","maps","mapdata",
+  "wordcloud","abind","SDMTools","PBSmapping","MASS",
+  "shiny","parallel","lubridate","rmarkdown","knitr",
+  "mvtnorm","ggplot2","kableExtra","DT")
+
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+ipak(packages)
+
